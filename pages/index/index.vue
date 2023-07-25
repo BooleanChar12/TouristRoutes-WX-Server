2061b3610e2b7f494765b318f83e972bc8bd3747
汪紫悦 <print_wzy@163.com>
2023-07-25 19:38:46 +0800

    111

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,42 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="box">
+			<view class="box-img">
+				<image src="/static/img/index/fengjing (1).png"></image>
+			</view>
+			<view class="box-content">
+				<view class="title">暑期-夏日青海湖</view>
+				<view class="abstract">热情的服务，路线安排的很合理；现在活动优惠力度和游玩...... </view>
+				<view class="message">
+					<view>5人 | 7天 </view>
+					<view>2023-6-6接团</view>
+				</view>
+				<view class="other">
+					<view class="money">总金额：￥680</view>
+					<view class="button">
+						<button @click="goToOrderIng">查看详情</button>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="box">
+			<view class="box-img">
+				<image src="/static/img/index/fengjing (2).png"></image>
+			</view>
+			<view class="box-content">
+				<view class="title">都江堰避暑山庄</view>
+				<view class="abstract">都江堰适合避暑的景区，山清水秀，远离城市喧嚣，适合自驾前往...... </view>
+				<view class="message">
+					<view>8人 | 15天</view>
+					<view>2023-6-24接团</view>
+				</view>
+				<view class="other">
+					<view class="money">总金额：￥1200</view>
+					<view class="button">
+						<button @click="goToOrderIng">查看详情</button>
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,42 +45,86 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			goToOrderIng(){
+				uni.navigateTo({
+					url:"/pages/order-ing/order-ing"
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		.box{
+			width: 690rpx;
+			background: #FCFCFC;
+			border: 2px solid #F2F2F2; // 边框 尺寸 样式（实线） 颜色
+			border-radius: 15rpx; // 边框圆角半径
+			display: flex;
+			margin-bottom: 20px;
+			// flex-direction: row; /* 将布局改为行方向 */
+			// align-items: center;
+			.box-img{
+				image{
+					width:280rpx;
+					height: 270rpx;
+					margin: 15rpx;
+				}
+			}
+			.box-content{
+				margin: 15rpx;
+				flex-direction: column;
+				.abstract{
+					font-size: 20rpx;
+					margin-top: 25rpx;
+					color:#6E6E6E;
+				}
+				.message{
+					display: flex;
+					flex-direction: row;
+					margin-top: 25rpx;
+					font-size: 26rpx;
+					view{
+						margin-right: 35rpx;
+					}
+				}
+				.other{
+					display: flex;
+					flex-direction: row;
+					margin-top: 25rpx;
+					.money{
+						margin-top: 11rpx;
+						margin-right: 25rpx;
+						color: #FE7B41;
+						font-size: 23rpx;
+					}
+					.button{
+						button{
+							display: flex;
+							align-items: center;
+							justify-content: center;
+							width: 160rpx;
+							height: 53rpx;
+							font-size: 23rpx;
+							background-color:#FF5303;
+							color:#FFFFFF;
+							border-radius: 30rpx;
+						}
+					}
+				}
+			}
+		}
 	}
 </style>
---
+++ pages/order-ing/order-ing.vue
@@ -0,0 +1,126 @@
+<template>
+	<view class="content">
+		<view class="box">
+			<view class="title">杭州西湖景观之旅</view>
+			<view class="description">这次我们将前往的是江南名胜杭州西湖~，同城向导随行讲解，贴心服务，车接车送。</view>
+		</view>
+		<view class="tag">
+			<button>标签1</button>
+			<button>标签2</button>
+			<button>标签3</button>
+		</view>
+		<view class="box">
+			<view class="meassage">
+				<view class="title">时间 :</view>
+				<view class="description">2023-6—5~2023-6-7</view>
+			</view>
+			<view class="meassage">
+				<view class="title">人数 :</view>
+				<view class="description">5人</view>
+			</view>
+			<view class="meassage">
+				<view class="title">司机 :</view>
+				<view class="description">刘师傅136****5134</view>
+			</view>
+			<view class="meassage">
+				<view class="title">酒店 :</view>
+				<view class="description">喜来登酒店 010-8989*****</view>
+			</view>
+		</view>
+		<view class="main-title">
+			<view class="title">行程服务进度</view>
+			<u-icon name="map" color="#6E6E6E"></u-icon>
+			<view class="description">总进度 :50%</view>
+		</view>
+		<view class="box">
+
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "uview-ui/index.scss";
+	.content {
+		display: flex;
+		flex-direction: column;
+		margin: 20rpx;
+
+		// align-items: center;
+		// justify-content: center;
+		.box {
+			width: 690rpx;
+			background: #FCFCFC;
+			border: 2px solid #F2F2F2;
+			border-radius: 15rpx;
+
+			.meassage {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				// justify-content: center;
+				margin-left: 20rpx;
+			}
+
+			.title {
+				margin: 20rpx;
+			}
+
+			.description {
+				margin: 20rpx;
+				color: #6E6E6E;
+				font-size: 25rpx;
+			}
+		}
+
+		.tag {
+			display: flex;
+			flex-direction: row;
+
+			button {
+				margin: 15rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				width: 160rpx;
+				height: 53rpx;
+				font-size: 25rpx;
+				background-color: #FF5303;
+				color: #FFFFFF;
+				border-radius: 30rpx;
+			}
+		}
+
+		.main-title {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			margin-top: 10rpx;
+
+			.title {
+				margin: 20rpx;
+				margin-right: 290rpx;
+				font-weight: bold;
+			}
+
+			.description {
+				margin: 13rpx;
+				color: #6E6E6E;
+			}
+			image {
+				width: 50rpx;
+				height: 50rpx;
+				margin-right: 1rpx;
+			}
+		}
+	}
+</style>
